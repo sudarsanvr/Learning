4e2117d3e2cf8a4eca7a194febcce0135226ce8b
sudarsan <sudarsan@DESKTOP-TVV01JP>
2018-01-26 00:08:35 +0530

    Page Object Model Framework

--- gittest/src/main/java/guru99/gittest/PG1.java
+++ gittest/src/main/java/guru99/gittest/PG1.java
@@ -1,19 +1,19 @@
 package guru99.gittest;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 //comment the above line and uncomment below line to use Chrome
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class PG1 {
 
 
     public static void main(String[] args) {
         // declaration and instantiation of objects/variables
-    	System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+    	//System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
 		//comment the above 2 lines and uncomment below 2 lines to use Chrome
 		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
     	
         String baseUrl = "http://demo.guru99.com/selenium/newtours/";
         String expectedTitle = "Welcome: Mercury Tours";
